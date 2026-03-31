b53a9e7e937c5dc066c6b1bfac3724fbddf354b8
Анна Куракина <Kurakina.A@edu.rea.ru>
2026-03-31 23:22:36 +0300

    Кастомная модель User, Teacher, Student, миграции

--- core/models.py
+++ core/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
