8daaf8a019fcf546d87078d6f2a670ecf92b8a8b
kurakina-a <anna.vnrcht@mail.ru>
2026-05-16 12:23:38 +0300

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 from .models import User, Test, Question, Option
 import re
 
@@ -54,18 +55,20 @@ class CustomUserCreationForm(forms.ModelForm):
 
 
 class TestForm(forms.ModelForm):
-    questions_data = forms.CharField(
-        required=False,
-        widget=forms.HiddenInput()
-    )
-
     class Meta:
         model = Test
-        fields = ['title', 'description']
+        fields = ['title', 'description', 'deadline']
         widgets = {
+            'deadline': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
             'title': forms.TextInput(attrs={'placeholder': 'Название теста'}),
             'description': forms.Textarea(attrs={'placeholder': 'Описание теста', 'rows': 3}),
         }
+
+    def clean_deadline(self):
+        deadline = self.cleaned_data.get('deadline')
+        if deadline and deadline < timezone.now():
+            raise forms.ValidationError("Дедлайн не может быть в прошлом!")
+        return deadline
 
 
 class QuestionForm(forms.ModelForm):
