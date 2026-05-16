ac64f60b479b273bf4dd7a8ae83474e8ec644d1a
Анна Куракина <Kurakina.A@edu.rea.ru>
2026-05-16 12:41:42 +0300

    Добавлено поле deadline в модель Test

--- core/models.py
+++ core/models.py
@@ -17,11 +17,13 @@ class Student(models.Model):
 
 class Test(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название теста')
-    description = models.TextField(blank=True, verbose_name='Описание') #описание теста (true - может быть пустым)
-    teacher = models.ForeignKey('User', on_delete=models.CASCADE, related_name='tests') #связь с User, CASCADE - если удалить учителя, удалится тест
-    created_at = models.DateTimeField(auto_now_add=True) #дата и время создания записываются автоматически
-    def __str__(self): 
-        return self.title  #при выводе вместо Test object будет написано название теста
+    description = models.TextField(blank=True, verbose_name='Описание')
+    deadline = models.DateTimeField(null=True, blank=True, verbose_name='Дедлайн')  
+    teacher = models.ForeignKey('User', on_delete=models.CASCADE, related_name='tests')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 class Question(models.Model):
     QUESTION_TYPES = [
